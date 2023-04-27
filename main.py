9f5d243536310c9ebcc9fc18d994a42789f81291
Julianapvh <70443877+julianapvh@users.noreply.github.com>
2023-04-27 19:54:30 -0400

    update das opções

---
+++ main.py
@@ -0,0 +1,32 @@
+def saudacao(boas_vindas):
+  print('Olá seja bem vindo(a)a SQUAD 03')
+while True:
+ opcoes = (input('Escolha uma opção: \n1 - Entrega \n2 - Pedidos e Pagamentos \n3 - Trocas e Devolução \n4 - Produtos \n5 - Cadastro \n6 - Sair'))
+ if opcoes == '1':
+  opcoes_entrega = (input('Escolha uma opção: \n1 - Como rastrear meu pedido? \n2 - Qual o prazo de entrega do meu pedido? \n3 - Como alterar meu endereço de entrega?'))
+  if opcoes_entrega == '1':
+    print('Copie o código de rastreio, recebido em seu e-mail e cole no site dos correios.')
+  elif opcoes_entrega == '2':
+   print('O prazo de entrega vai depender do seu estado.')
+   
+  elif opcoes_entrega == '3':
+   print('Entre no site e altere o endereço.')
+   
+
+ 
+  break
+  continue
+  
+ else:
+  print('Escolha uma opção válida!')
+    
+
+
+ 
+  
+ 
+  
+
+
+
+
