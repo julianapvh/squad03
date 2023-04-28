a5614a64af07fa23338bde757e6555855f06ae1c
Danylo Miranda <131209067+ninylo@users.noreply.github.com>
2023-04-28 10:01:21 -0300

    Update Indentação
    Atualização da indentação do código para os padrões de boas práticas da comunidade python, ajuste nas caixas de opções colocando os números do menu dentro de  "[ ]" e inclusão de "\n" no final de cada input.

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
 def saudacao(boas_vindas):
   print('Olá seja bem vindo(a)a SQUAD 03')
-while True:
- opcoes = (input('Escolha uma opção: \n1 - Entrega \n2 - Pedidos e Pagamentos \n3 - Trocas e Devolução \n4 - Produtos \n5 - Cadastro \n6 - Sair'))
- if opcoes == '1':
-  opcoes_entrega = (input('Escolha uma opção: \n1 - Como rastrear meu pedido? \n2 - Qual o prazo de entrega do meu pedido? \n3 - Como alterar meu endereço de entrega?'))
-  if opcoes_entrega == '1':
-    print('Copie o código de rastreio, recebido em seu e-mail e cole no site dos correios.')
-  elif opcoes_entrega == '2':
-   print('O prazo de entrega vai depender do seu estado.')
-   
-  elif opcoes_entrega == '3':
-   print('O prazo de entrega vai depender do seu estado.')
-   
-  elif opcoes_entrega == '3':
-   print('Entre no site e altere o endereço.')
-   
-
- 
-  break
-  continue
   
- else:
-  print('Escolha uma opção válida!')
+while True:
+    opcoes = (input('Escolha uma opção: \n[1] - Entrega \n[2] - Pedidos e Pagamentos \n[3] - Trocas e Devolução \n[4] - Produtos \n[5] - Cadastro \n[6] - Sair \n'))
+    if opcoes == '1':
+        opcoes_entrega = (input('Escolha uma opção: \n[1] - Como rastrear meu pedido? \n[2] - Qual o prazo de entrega do meu pedido? \n[3] - Como alterar meu endereço de entrega? \n'))
+    if opcoes_entrega == '1':
+        print('Copie o código de rastreio, recebido em seu e-mail e cole no site dos correios.')
+    elif opcoes_entrega == '2':
+        print('O prazo de entrega vai depender do seu estado.')   
+    elif opcoes_entrega == '3':
+        print('O prazo de entrega vai depender do seu estado.')   
+    elif opcoes_entrega == '3':
+        print('Entre no site e altere o endereço.')
+    break
+else:
+    print('Escolha uma opção válida!')
     
 
 
