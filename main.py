8ee29fc56d4bb6f1261f52748987aa6854432749
juliana gomes da silva <julianacore2@gmail.com>
2023-04-28 10:18:40 -0400

    update de outras opções
    foi adicionado mais opções para o bot

--- main.py
+++ main.py
@@ -1,27 +1,111 @@
-def saudacao(boas_vindas):
-  print('Olá seja bem vindo(a)a SQUAD 03')
+print('Olá seja bem vindo(a) a SQUAD 03')
+
+
 while True:
- opcoes = (input('Escolha uma opção: \n1 - Entrega \n2 - Pedidos e Pagamentos \n3 - Trocas e Devolução \n4 - Produtos \n5 - Cadastro \n6 - Sair'))
+ 
+ opcoes = (input('Escolha uma opção: \n1 - Entrega  \n2 - Pedidos e Pagamentos  \n3 - Trocas e Devolução  \n4 - Produtos  \n5 - Cadastro  \n6 - Sair '))
+ 
  if opcoes == '1':
-  opcoes_entrega = (input('Escolha uma opção: \n1 - Como rastrear meu pedido? \n2 - Qual o prazo de entrega do meu pedido? \n3 - Como alterar meu endereço de entrega?'))
+  opcoes_entrega = (input('Escolha uma opção: \n1 - Como rastrear meu pedido?  \n2 - Qual o prazo de entrega do meu pedido?  \n3 - Como alterar meu endereço de entrega? \n'))
+  
   if opcoes_entrega == '1':
-    print('Copie o código de rastreio, recebido em seu e-mail e cole no site dos correios.')
+    print('Dentro de “Minha conta” clique em “Meus pedidos” e procure pela sua comprinha. Elas estão organizadas pelo número de identificação. Para ver mais detalhes clique em VER DETALHES.\nO código de rastreio deve aparecer logo abaixo do resumo da sua compra. Mas lembre-se que ele aparece de formas diferentes dependendo da forma de entrega que você selecionou.\nCom o código de rastreio em mãos, basta você localizar a sua comprinha no site dos Correios.')
+
+    
   elif opcoes_entrega == '2':
    print('O prazo de entrega vai depender do seu estado.')
    
-  elif opcoes_entrega == '3':
-   print('O prazo de entrega vai depender do seu estado.')
+
    
   elif opcoes_entrega == '3':
-   print('Entre no site e altere o endereço.')
+   print('Os Correios não permitem que a gente altere nenhuma informação após o despacho da sua comprinha. Por isso, vamos ter que aguardar a sua comprinha retornar para a nossa loja para um novo despacho..')
    
 
- 
-  break
-  continue
   
- else:
-  print('Escolha uma opção válida!')
+  
+ 
+  
+ #================================================================================================================================================================================================================================================
+ if opcoes == '2':
+   
+  opcoes_pedidos_pagamentos = (input('Escolha uma opção: \n1 - Qual o prazo de aprovação do pedido?  \n2 - Quais são as formas de pagamentos disponíveis?  \n3 - Como faço pra conseguir a 2° via do boleto? \n'))
+ 
+ 
+    
+  
+  if opcoes_pedidos_pagamentos == '1':
+    print('')
+          
+    
+  elif opcoes_pedidos_pagamentos == '2':
+   print('')
+   
+
+   
+  elif opcoes_pedidos_pagamentos == '3':
+   print('')
+   
+   
+  
+  
+  
+   #==========================================================================================================================================================================================================
+   
+  if opcoes == '3':
+     opcoes_trocas_devolucoes = (input('Escolha uma opção: \n1 - Recebi meu pedido incompleto ou danificado.  \n2 - Como funciona a política de troca e devolução?  \n3 - Me arrependi da compra. E agora? \n'))
+ 
+  if opcoes_trocas_devolucoes == '1':
+    print('')
+          
+    
+  elif opcoes_trocas_devolucoes == '2':
+    print('')
+   
+
+   
+  elif opcoes_trocas_devolucoes == '3':
+    print('')
+ 
+ 
+ 
+ #========================================================================================================================================================================================================
+ 
+ 
+ if opcoes == '4':
+   opcoes_produtos = (input('Escolha uma opção: \n1 - Estou com dúvida sobre o produto. Onde tenho mais informações?  \n2 - O produto que procuro não está disponível, o que faço?  \n3 - Posso retirar meus produtos em alguma loja física? \n'))
+ 
+ if opcoes_produtos == '1':
+   print('')
+          
+    
+ elif opcoes_produtos == '2':
+   print('')
+   
+
+   
+ elif opcoes_produtos == '3':
+   print('')
+ 
+ #========================================================================================================================================================================================================================================================================
+ 
+ 
+ if opcoes == '5':
+   opcoes_cadastro = (input('Escolha uma opção: \n1 - Como fazer meu cadastro?  \n2 - Esqueci minha senha, e agora?  \n3 - Como alterar ou editar  dados pessoais cadastrados no site? \n'))
+  
+ if opcoes_cadastro == '1':
+    print('')
+          
+    
+ elif opcoes_cadastro == '2':
+   print('')
+   
+
+   
+ elif opcoes_cadastro == '3':
+   print('')
+ 
+ 
+
     
 
 
