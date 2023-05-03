3fdc11e5fefcec734aaa2615b67c795f141ced96
Gideoni <115567537+gideoni-santos@users.noreply.github.com>
2023-05-03 19:39:51 -0300

    att bug do danylo
    danylo achou um bug cabuloso, ficamos um tempo tentando resolver, e no final era uma "" e tirar o int do input.

--- funcoes.py
+++ funcoes.py
@@ -8,11 +8,11 @@
     
 def menu_voltar_sair():
     while True:
-        opcao_voltar_sair = int(input("\nDigite [1] para encerrar o atendimento ou [2] para voltar ao menu anterior: "))
-        if opcao_voltar_sair == 1:
+        opcao_voltar_sair = input("\nDigite [1] para encerrar o atendimento ou [2] para voltar ao menu anterior: ")
+        if opcao_voltar_sair == "1":
             print("Obrigado por usar nosso atendimento. Volte sempre!")
             exit()
-        elif opcao_voltar_sair == 2:
+        elif opcao_voltar_sair == "2":
             break
         else:
             print("Insira uma opção válida")
