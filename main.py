f57696f8740339cbb4d505e529310decd53ca5b8
Gideoni <115567537+gideoni-santos@users.noreply.github.com>
2023-05-03 16:04:54 -0300

    Revert "Merge branch 'main' of https://github.com/julianapvh/squad03"
    This reverts commit 630fe290d97e33ee9250accdb2f99e8624a3006f, reversing
    changes made to 3fd374b1a667d570007ce9854ff4b1fa4cbf2daa.

--- main.py
+++ main.py
@@ -18,20 +18,20 @@ while True:
         
 
         if opcoes_entrega == '1':
-            print('''Dentro de “Minha conta” clique em “Meus pedidos” e procure pela sua comprinha.
-                Elas estão organizadas pelo número de identificação. Para ver mais detalhes clique em VER DETALHES.
+            print('''\nDentro de “Minha conta” clique em “Meus pedidos” e procure pela sua comprinha.
+                \nElas estão organizadas pelo número de identificação. Para ver mais detalhes clique em VER DETALHES.
                 \nO código de rastreio deve aparecer logo abaixo do resumo da sua compra.
-                Mas lembre-se que ele aparece de formas diferentes dependendo da forma de entrega que você selecionou.
+                \nMas lembre-se que ele aparece de formas diferentes dependendo da forma de entrega que você selecionou.
                 \nCom o código de rastreio em mãos, basta você localizar a sua comprinha no site dos Correios.
                 ''')
 
         elif opcoes_entrega == '2':
-            print('O prazo de entrega vai depender do seu estado.')
+            print('\nO prazo de entrega vai depender do seu estado.')
 
         elif opcoes_entrega == '3':
-            print('''Os Correios não permitem que a gente altere nenhuma informação após o despacho da sua comprinha. 
-                Por isso, vamos ter que aguardar a sua comprinha retornar para a nossa loja para um novo despacho.
-                ''')
+            print('''\nOs Correios não permitem que a gente altere nenhuma informação após o despacho da sua comprinha.
+                  \nPor isso, vamos ter que aguardar a sua comprinha retornar para a nossa loja para um novo despacho. 
+                  ''')
         else:
              opcoes_entrega == '4'
              retornar_ao_menu_anterior
@@ -51,20 +51,20 @@ while True:
                 \n[3] - Como faço pra conseguir a 2° via do boleto? \n[4] - Voltar ao menu anterior '))
 
         if opcoes_pedidos_pagamentos == '1':
-            print('''Isso vai depender da sua forma de pagamento. 
-                Se for cartão de crédito ou pix a compra será aprovada na mesma hora, 
-                    já se for via boleto bancário a aprovação será aprovada entre 1 e 2 dia úteis
+            print('''\nIsso vai depender da sua forma de pagamento. 
+                \nSe for cartão de crédito ou pix a compra será aprovada na mesma hora, 
+                \njá se for via boleto bancário a aprovação será aprovada entre 1 e 2 dia úteis
                 ''')
 
         elif opcoes_pedidos_pagamentos == '2':
             print(
-                'Você pode pagar suas compras nos cartões de débito e crédito ou via pix.')
+                '\nVocê pode pagar suas compras nos cartões de débito e crédito ou via pix.')
 
         elif opcoes_pedidos_pagamentos == '3':
-            print('''Para regerar seu boleto na loja do SQUAD 03 é preciso acessar o site da loja e clicar no menu superior que levará aos seus pedidos.
-                Aí você entra com seu login e a respectiva senha do cadastro para ter acesso às opções da área do cliente. 
+            print('''\nPara regerar seu boleto na loja do SQUAD 03 é preciso acessar o site da loja e clicar no menu superior que levará aos seus pedidos.
+                \nAí você entra com seu login e a respectiva senha do cadastro para ter acesso às opções da área do cliente. 
                 \nUma vez logado no sistema, vá até o menu de exibição de detalhes e escolha a opção para imprimir a 2ª via do boleto. 
-                Prontinho: já regerou sua 2° via!
+                \nProntinho: já regerou sua 2° via!
                 ''')
             
             
@@ -87,44 +87,44 @@ while True:
                 \n[3] - Me arrependi da compra. E agora? \n[4] - Voltar ao menu anterior '))
 
         if opcoes_trocas_devolucoes == '1':
-            print('''Você tem até 7 dias após o recebimento do seu pedido para abrir uma solicitação de devolução via site SQUAD 03. 
+            print('''\nVocê tem até 7 dias após o recebimento do seu pedido para abrir uma solicitação de devolução via site SQUAD 03. 
                 \nPara investigarmos corretamente a sua situação, costumamos solicitar algumas provas que vão nos dizer porque a sua 
-                  experiência não foi positiva com a compra e como podemos melhorar. 
+                \nexperiência não foi positiva com a compra e como podemos melhorar. 
                 \nGeralmente, pedimos que você anexe à sua solicitação de reembolso imagens que justifiquem o motivo do reembolso: 
                 \nFotos do item recebido:
                 \nErrado: item recebido de cor/tamanho diferentesDanificado: 
                 \nembalagem danificada, itens quebrados ou com danos (amassados, riscos)Com defeito: 
-                  é comum que seja algum eletrônico, por isso, pedimos que você nos forneça alguma captura de tela 
-                  com o dispositivo plugado que mostre qual é o problemaIncompleto: imagem com todos os itens recebidos no pedido -
+                \né comum que seja algum eletrônico, por isso, pedimos que você nos forneça alguma captura de tela 
+                \ncom o dispositivo plugado que mostre qual é o problemaIncompleto: imagem com todos os itens recebidos no pedido -
                 \ncaso esteja faltando alguma peça ou componente 
                 \nHistórico do chat ou qualquer outra evidência que nos mostre alguma negociação prévia com o vendedor (se houver)
                 ''')
 
         elif opcoes_trocas_devolucoes == '2':
-            print('''Política de Trocas e Devoluções  
+            print('''\nPolítica de Trocas e Devoluções  
                     \nTroca por defeito de fabricação:
-                    Caso o produto apresente defeito de fabricação em até 30 dias corridos a partir dos dados de recebimento do produto,
-                    o cliente deverá entrar em contato com nossa central de atendimento e solicitar a troca do produto.
-                    O produto deve estar em perfeitas condições, sem sinais de uso, e com embalagem original. 
-                    O valor do frete da devolução do produto e da entrega
+                    \nCaso o produto apresente defeito de fabricação em até 30 dias corridos a partir dos dados de recebimento do produto,
+                    \no cliente deverá entrar em contato com nossa central de atendimento e solicitar a troca do produto.
+                    \nO produto deve estar em perfeitas condições, sem sinais de uso, e com embalagem original. 
+                    \nO valor do frete da devolução do produto e da entrega
                     \nDevolução por produto com defeito após 30 dias : 
-                    Caso o produto apresente defeito após 30 dias corridos a partir dos dados do destinatário do produto, o cliente deverá entrar em contato 
-                    com o fabricante para acionar a garantia. A loja não responderá
+                    \nCaso o produto apresente defeito após 30 dias corridos a partir dos dados do destinatário do produto, o cliente deverá entrar em contato 
+                    \ncom o fabricante para acionar a garantia. A loja não responderá
                     \nDevolução por produto diferente do solicitado : 
-                    Caso o cliente receba um produto diferente do solicitado, deverá entrar em contato com a nossa central de 
-                    atendimento em até 7 dias corridos a partir dos dados de recebimento do produto. A loja será responsável 
-                    pelo envio do produto correto e pelo pagamento do frete da devolução do produto incorreto.''')
+                    \nCaso o cliente receba um produto diferente do solicitado, deverá entrar em contato com a nossa central de 
+                    \natendimento em até 7 dias corridos a partir dos dados de recebimento do produto. A loja será responsável 
+                    \npelo envio do produto correto e pelo pagamento do frete da devolução do produto incorreto.''')
 
         elif opcoes_trocas_devolucoes == '3':
-            print('''Devolução por arrependimento: Caso o cliente se arrependa da compra e queira devolver o produto, deverá entrar em contato 
-                com a nossa central de atendimento em até 7 dias corridos a partir dos dados de recebimento do produto. 
-                O produto deve estar em perfeitas condições, sem sinais de uso, e com embalagem original. 
-                O valor do produto será estornado, mas o valor do frete não será reembolsado. 
+            print('''\nDevolução por arrependimento: Caso o cliente se arrependa da compra e queira devolver o produto, deverá entrar em contato 
+                \ncom a nossa central de atendimento em até 7 dias corridos a partir dos dados de recebimento do produto. 
+                \nO produto deve estar em perfeitas condições, sem sinais de uso, e com embalagem original. 
+                \nO valor do produto será estornado, mas o valor do frete não será reembolsado. 
                 \nTroca por insatisfação: 
-                Caso o cliente fique insatisfeito com o produto, mas 
-                ele esteja em perfeitas condições, sem sinais de uso, e com a embalagem original, poderá solicitar a troca do produto 
-                em até 30 dias corridos a partir dos dados de recebimento do produto . 
-                O valor do frete da devolução do produto e da entrega do novo produto será de responsabilidade do cliente.
+                \nCaso o cliente fique insatisfeito com o produto, mas 
+                \nele esteja em perfeitas condições, sem sinais de uso, e com a embalagem original, poderá solicitar a troca do produto 
+                \nem até 30 dias corridos a partir dos dados de recebimento do produto . 
+                \nO valor do frete da devolução do produto e da entrega do novo produto será de responsabilidade do cliente.
                 ''')
         else:
             opcoes_trocas_devolucoes == '4'
@@ -145,15 +145,13 @@ while True:
 
         if opcoes_produtos == '1':
             print(
-                'Você pode entrar em contato direto com a nossa central de atendimento pelo SAC 2.0 X 3.0 X 4.0')
+                '\nVocê pode entrar em contato direto com a nossa central de atendimento pelo SAC 2.0 X 3.0 X 4.0')
 
         elif opcoes_produtos == '2':
-            print('Você pode adicionar o produto a sua lista de desejos e quando o item estiver disponivel você receberá um e-mail informando que \
-                o item está disponivel para venda em nosso site.')
+            print('\nVocê pode adicionar o produto a sua lista de desejos e quando o item estiver disponivel você receberá um e-mail informando que o item está disponivel para venda em nosso site.')
 
         elif opcoes_produtos == '3':
-            print('Sim, nós contamos com diversas filiais espalhadas pelo Brasil. Você pode escolher a mais próxima de você para retirar \
-                seus produtos.')
+            print('\nSim, nós contamos com diversas filiais espalhadas pelo Brasil. Você pode escolher a mais próxima de você para retirar seus produtos.')
             
         else:
             opcoes_produtos == '4'
@@ -173,7 +171,7 @@ while True:
                 \n[3] - Como alterar ou editar  dados pessoais cadastrados no site? \n[4] - Voltar ao menu anterior '))
 
         if opcoes_cadastro == '1':
-            print('''1 - Visite o site squad03.com No canto superior direito da tela, você verá um menu marcado como “Minha Conta.” 
+            print('''\n1 - Visite o site squad03.com No canto superior direito da tela, você verá um menu marcado como “Minha Conta.” 
                 Quando você passar o mouse sobre ele, um menu suspenso irá surgir. Clique no link "Comece Aqui/Start Here"
                 logo abaixo do botão azul "Inscreva-se/Sign In". 
                 \n2 - Insira suas informações pessoais. O formulário de registro vai pedir seu nome e endereço de e-mail, 
@@ -186,7 +184,7 @@ while True:
                 ''')
 
         elif opcoes_cadastro == '2':
-            print('''Redefinir sua senha 
+            print('''\nRedefinir sua senha 
                 \nSe você esqueceu sua senha, poderá redefini-la usando o processo de recuperação de senha do nosso site.
                 \nPara redefinir sua senha: 
                 \nAcesse aqui squad03/ap/forgotpassword?. 
@@ -198,7 +196,7 @@ while True:
                 ''')
 
         elif opcoes_cadastro == '3':
-            print('''Alterar as configurações da conta 
+            print('''\nAlterar as configurações da conta 
                     \nVocê pode atualizar seu nome, endereço de e-mail, senha ou outras informações da conta. 
                     \nPara alterar as configurações da conta: 
                     \nEm Sua conta, acesse Acesso e segurança. 
