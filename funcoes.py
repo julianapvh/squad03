b6f8263250db6b68e8c250fe49ab657311aef99f
Danylo Miranda <danylo.s.miranda@gmail.com>
2023-05-04 19:07:02 -0300

    Remoção de comentários
    Removi comentários que estavam no inicio do arquivo funções

--- funcoes.py
+++ funcoes.py
@@ -1,11 +1,3 @@
-# def retornar_ao_menu_anterior(options):
-
-#     return opcoes
-
-# def mensagem_de_erro():
-#     excecao = [OSError('Erro 1'), ('Você digitou algo diferente das opções disponíveis, favor digite um número referente a uma opção...')]
-#     raise ExceptionGroup('there were problems', excecao)
-    
 def menu_voltar_sair():
     while True:
         opcao_voltar_sair = input("\nDigite [1] para encerrar o atendimento ou [2] para voltar ao menu anterior: ")
